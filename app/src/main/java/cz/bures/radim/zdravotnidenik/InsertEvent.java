9ffff095341c4c6770cfcdf3fa838add542f23ff
Radim <radim.bures@gmail.com>
2014-12-05 00:17:24 +0100

    close button

--- app/src/main/java/cz/bures/radim/zdravotnidenik/InsertEvent.java
+++ app/src/main/java/cz/bures/radim/zdravotnidenik/InsertEvent.java
@@ -1,9 +1,11 @@
 package cz.bures.radim.zdravotnidenik;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class InsertEvent extends Activity {
@@ -12,6 +14,11 @@ public class InsertEvent extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_event);
+    }
+
+    public void onCancel(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
 
